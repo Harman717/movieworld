7ee3a913fff504560134fce312e73d75b49b3b29
Harmandeep Singh <76901738+Harman717@users.noreply.github.com>
2024-06-14 22:58:21 +0530

    Added UseEffect Hook

--- src/App.js
+++ src/App.js
@@ -73,13 +73,16 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchMovies() {
       try {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`
+          `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
         );
         if (!res.ok)
           throw new Error("Something went wrong with fetching movies..");
@@ -89,7 +92,7 @@ export default function App() {
         setIsLoading(false);
       } catch (err) {
         console.log(err.message);
-        setError(err.message);
+        if (err.name !== "AbortError") setError(err.message);
       } finally {
         setIsLoading(false);
       }
@@ -99,6 +102,9 @@ export default function App() {
       setError("");
     }
     fetchMovies();
+    return function () {
+      controller.abort();
+    };
   }, [query]);
 
   return (
@@ -258,6 +264,20 @@ function SelectedMovie({ SelectedId, onCloseMovie, onAddWatched, watched }) {
   }
   useEffect(
     function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+  useEffect(
+    function () {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
@@ -272,6 +292,16 @@ function SelectedMovie({ SelectedId, onCloseMovie, onAddWatched, watched }) {
       getMovieDetails();
     },
     [SelectedId]
+  );
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+      return function () {
+        document.title = "MovieWorld";
+      };
+    },
+    [title]
   );
   return (
     <div className="details">
